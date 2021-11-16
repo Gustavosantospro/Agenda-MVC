ba53781ef18c39a11c15d05aa8887ae2211c041e
Gustavo Vieira <60290436+Gustavosantospro@users.noreply.github.com>
2021-11-16 16:32:30 -0300

    Update Logica.java

--- Agenda/src/br/com/githubGustavoSantos/agenda/mvc/logica/Logica.java
+++ Agenda/src/br/com/githubGustavoSantos/agenda/mvc/logica/Logica.java
@@ -1,4 +1,4 @@
-package br.com.caelum.agenda.mvc.logica;
+package br.com.githubGustavoSantos.agenda.mvc.logica;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
