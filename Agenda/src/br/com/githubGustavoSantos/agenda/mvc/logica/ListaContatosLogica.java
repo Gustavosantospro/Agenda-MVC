6cc293a3d4a6f35bef9f9586edb990a1cf79aeee
Gustavo Vieira <60290436+Gustavosantospro@users.noreply.github.com>
2021-11-16 16:31:59 -0300

    Update ListaContatosLogica.java

--- Agenda/src/br/com/githubGustavoSantos/agenda/mvc/logica/ListaContatosLogica.java
+++ Agenda/src/br/com/githubGustavoSantos/agenda/mvc/logica/ListaContatosLogica.java
@@ -1,12 +1,12 @@
-package br.com.caelum.agenda.mvc.logica;
+package br.com.githubGustavoSantos.agenda.mvc.logica;
 
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import br.com.caelum.agenda.dao.ContatoDao;
-import br.com.caelum.agenda.modelo.Contato;
+import br.com.githubGustavoSantos.agenda.dao.ContatoDao;
+import br.com.githubGustavoSantos.agenda.modelo.Contato;
 
 public class ListaContatosLogica implements Logica {
 	@Override
